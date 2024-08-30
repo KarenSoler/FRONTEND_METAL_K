7047d18ba968ab4bcfede396ee457bf0121e3519
SantherEHPM <erickhernandop@gmail.com>
2024-08-30 01:33:33 -0500

    Push config by bat

---
+++ .github/pushes/push-config.bat
@@ -0,0 +1,27 @@
+@echo off
+echo.
+git add .\.github\*
+git add .\package.json
+git add .\svelte.config.js
+git add .\.env
+git status -s 
+echo .
+color F
+SET /p confirm= ^> Realizar commit(Y/N):
+
+if "%confirm%"=="Y" goto commit
+if "%confirm%"=="N" goto cancel
+
+:commit
+set /p commit=^>Nombre del commit 
+echo.
+git commit -m "%commit%"
+SET /p push= ^> Realizar commit(Y/N):
+
+if "%push%"=="Y" goto push
+if "%push%"=="N" goto cancel
+
+:push
+git push origin HEAD
+
+:cancel
