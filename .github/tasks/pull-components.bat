ba9646d0f4d1f199c77b41de21557a4657dacc81
Santher <erickhernandop@gmail.com>
2024-08-30 10:28:29 -0500

    Pull components

--- .github/tasks/pull-components.bat
+++ .github/tasks/pull-components.bat
@@ -2,16 +2,13 @@
 echo.
 
 ::Files to catch changes
-git pull origin u/components
-git pull origin u/styles
-
+git fetch -a
 
 echo .
 SET /p confirm= ^> Realizar pull(Y/N):
 
 if "%confirm%"=="Y" goto pull
 if "%confirm%"=="N" goto cancel
-
 
 :pull
     git pull origin u/components
