8a530223d6fff0d30ba7c32a4a5d1b6497ea37a4
SantherEHPM <erickhernandop@gmail.com>
2024-08-30 01:47:04 -0500

    Push styles by .bat

--- .github/pushes/push-components.bat
+++ .github/pushes/push-components.bat
@@ -1,7 +1,11 @@
 @echo off
 echo.
+
+::Files to catch changes
 git add .\\src\\lib\\components\\* 
+
 git status -s 
+
 echo .
 color F
 SET /p confirm= ^> Realizar commit(Y/N):
@@ -11,10 +15,10 @@ if "%confirm%"=="N" goto cancel
 
 
 :commit
-set /p commit=^>Nombre del commit 
+set /p commit=^>Nombre del commit:
 echo.
 git commit -m "%commit%"
-SET /p push= ^> Realizar commit(Y/N):
+SET /p push= ^> Realizar push(Y/N):
 
 if "%push%"=="Y" goto push
 if "%push%"=="N" goto cancel
--- .github/pushes/push-config.bat
+++ .github/pushes/push-config.bat
@@ -1,9 +1,12 @@
 @echo off
 echo.
+
+::Files to catch changes
 git add .\.github\*
 git add .\package.json
 git add .\svelte.config.js
 git add .\.env
+
 git status -s 
 echo .
 color F
@@ -13,10 +16,10 @@ if "%confirm%"=="Y" goto commit
 if "%confirm%"=="N" goto cancel
 
 :commit
-set /p commit=^>Nombre del commit 
+set /p commit=^>Nombre del commit:
 echo.
 git commit -m "%commit%"
-SET /p push= ^> Realizar commit(Y/N):
+SET /p push= ^> Realizar push(Y/N):
 
 if "%push%"=="Y" goto push
 if "%push%"=="N" goto cancel
