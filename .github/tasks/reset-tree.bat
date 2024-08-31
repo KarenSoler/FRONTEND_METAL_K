93c458e3c2b1ff55a166e0193164071e8b801a14
Santher <erickhernandop@gmail.com>
2024-08-31 10:35:57 -0500

    Sync config

---
+++ .github/tasks/reset-tree.bat
@@ -0,0 +1,15 @@
+@echo off
+git log --oneline -n5  
+
+SET /p num= ^> A cual commit va a regresar:
+git reset --mixed %num%
+
+SET /p push= ^> Eliminar de origen(Y/N):
+
+if "%push%"=="Y" goto push
+if "%push%"=="N" goto cancel
+
+:push
+    git push origin HEAD --force
+
+:cancel
