75cb468044066311448ba2467a3704b9db597d3a
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-08-31 01:22:26 -0500

    Resposive Form 1

---
+++ src/routes/admin/login/+page.svelte
@@ -0,0 +1,43 @@
+<script lang='ts'>
+    import Tester from "$lib/components/Tester.svelte";
+    import Component from "../../test/component.svelte"
+</script>
+
+<header>
+    <Tester --width={"40em"} --color={"#0000FF"} --height={"auto"} class={'form'}>
+        <h1> Form</h1>
+        <Tester --width={"100%"} --color={"#2FFF"} --height={"4em"}>Input</Tester>
+        <Tester --width={"100%"} --color={"#2FFF"} --height={"4em"}>Input</Tester>
+        <Tester --width={"5em"} --color={"#2FFF"} --height={"3em"}>Submit</Tester>
+    </Tester>
+    
+</header>
+
+<style lang='sass' global>
+.form
+    display: flex
+
+    justify-content: center
+    align-items: center
+
+    background: #FFFFAA
+
+    padding: 2em
+
+
+//$width: var(--width)
+    //$height: var(--height)
+    //$color: var(--color)
+
+    /*.tester
+        display: inline-block
+        
+        width: $width
+        height: $height*/
+
+      //  border-color: $color
+
+      //  border-width: 5px
+      //  border-style: solid
+
+</style>
