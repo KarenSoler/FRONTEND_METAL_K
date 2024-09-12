1564d7991fdf4847d56e6d688e8a071358a305f1
SantherEHPM <erickhernandop@gmai.com>
2024-09-12 15:35:38 -0500

    Modal with redirect

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,52 +1,103 @@
-<script lang='ts'>
-    export let textModal = 'Texto';  // Texto personalizado para el modal
-    export let triggerModal = false; // Señal para abrir el modal
+<script lang='ts'>  // Texto personalizado para el modal
+    let triggerModal = false; // Señal para abrir el modal
+    let show:boolean = true; // Estado interno para mostrar el modal
+    let route:string|undefined = undefined
 
-    let showModal = false; // Estado interno para mostrar el modal
-
+    // let option = {
+    //     onclick:closeModal,
+    //     class:'modal-button'
+    // }
     // Función para abrir el modal cuando se recibe la señal
     $: if (triggerModal) {
-        showModal = true;
+        show = true;
     }
 
     // Función para cerrar el modal
     function closeModal() {
-        showModal = false; // Cerrar el modal
+        show = false; // Cerrar el modal
+    }
+
+    export{
+        route
     }
 </script>
 
-{#if showModal}
-    <div class="contenedor">
-        <p>{textModal}</p> 
-        <button class="modal-button" on:click={closeModal}>Cerrar</button>
+{#if show}
+    <div class='modal-bg'>
+        <div class="contenedor">
+            <p><slot/></p> 
+            <span class="modal-options">
+                <slot name='option'>
+                    {#if route}
+                        <a class="modal-button" on:click={closeModal} href={route}>
+                            <span>Aceptar</span>
+                        </a>
+                    {:else}
+                        <button class="modal-button" on:click={closeModal}>
+                            <span>Cerrar</span>
+                        </button>
+                    {/if}
+                </slot>
+            </span>
+        </div>
     </div>
 {/if}
 <style lang='sass'>
 
 @use 'src/lib/styles/palete.sass' as palete
+.modal-bg
+    position: absolute
+    top:0
+    left:0
 
-.contenedor
-    background: palete.$container
-    padding: 1em
-    border-radius: 1em
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2)
-    position: fixed
-    top: 50%
-    left: 50%
-    transform: translate(-50%, -50%)
-    width: 80%
-    max-width: 400px
-    text-align: center
-    z-index: 1000
+    width: 100vw
+    height: 100vh
 
-.modal-button
-    background: palete.$button
-    border: palete.$button-border
-    padding: 0.5em 1em
-    margin-top: 1em
-    cursor: pointer
+    background: palete.$modal-bg
 
-    &:hover
-        background: palete.$button, 10%
+
+    .contenedor
+        position: fixed
+        top: 50%
+        left: 50%
+
+        display: flex
+        flex-direction: column
+        gap: 0.5em
+
+        background: palete.$container
+        padding: 1em
+        border-radius: 1em
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2)
+        transform: translate(-50%, -50%)
+        width: 80%
+        max-width: 400px
+        text-align: center
+        z-index: 1000
+        .modal-options
+            display: flex
+            justify-content: center
+            flex-wrap: nowrap
+            gap:1em
+
+            width: 100%
+            height: fit-content
+
+            .modal-button
+                cursor: pointer
+
+                width: 100%
+
+                padding: 0.5em 1em
+
+                background: palete.$button
+
+                border: palete.$button-border
+                border-radius: 0.5em
+
+                font-size: 1em
+
+                &:hover
+                    background: palete.$button-hover
 
 </style>
