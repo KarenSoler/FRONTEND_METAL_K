5391d18d53160421bd84ad0860b8e44b13b89db0
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-09-24 22:31:37 -0500

    modal casi terminado

--- src/lib/components/user/ProductUser.svelte
+++ src/lib/components/user/ProductUser.svelte
@@ -2,69 +2,112 @@
     import { writable } from 'svelte/store';
     import image_list from "../../../lib/jsons/imageUser.json"
     import feature_list from "../../jsons/orderOptions.json"
+    import { onMount } from 'svelte';
 
-    let focus = false
-    let images:Array<Product> = image_list.images.map((image)=> {
-        return image;  // Cierra correctamente la función de retorno
+    
+    let images:Array<Product> = image_list.images.map((image) => {
+        return image
     })
 
-    let categories:Array<Category> = feature_list.categories.map((category)=>{
+    let categories:Array<Category> = feature_list.categories.map((category) => {
         return category
     })
+
+    let tags:Array<Tags> = feature_list.tags.map((tag) => {
+        return tag
+    })
+
+    // Producto seleccionado para mostrar en el modal (empieza en nulo)
+    let selectedImage: Product | null = null
+    
+    // Controla si se puede ver el modal 
+    let showModal: boolean = false
     
 
-    //Variable que asigna un estado "cerrado" a la información expansible
-    let expandedImage: number | null = null
+    // //Variable que asigna un estado "cerrado" a la información expansible
+    // let expandedImage: number | null = null
 
-    //metodo para colapsar la tarjeta comparando el id de una imagen expandida (expandedImage) con el id de la imagen que unopase y quiera colapsar (si son iguales toma como que está abierta y se colapsa)
-    //El toggle es porque alterna entre estados, si no cumple la condición, en lugar de colapsarla, la expande
-    function toggleImage(id: number) {
-        expandedImage = expandedImage === id ? null : id
+    // //metodo para colapsar la tarjeta comparando el id de una imagen expandida (expandedImage) con el id de la imagen que unopase y quiera colapsar (si son iguales toma como que está abierta y se colapsa)
+    // //El toggle es porque alterna entre estados, si no cumple la condición, en lugar de colapsarla, la expande
+    // function toggleImage(id: number) {
+    //     expandedImage = expandedImage === id ? null : id
+    // }
+
+    // Función para abrir el modal con los detalles de la imagen seleccionada
+    function openModal(image: Product) {
+        selectedImage = image;
+        showModal = true;
     }
 
+    // Función para cerrar el modal
+    function closeModal() {
+        selectedImage = null;
+        showModal = false;
+    }
 
+    // Obtener el nombre de la categoría basado en el ID
+    function getCategoryName(categoryId: number) {
+        let category = categories.find(cat => cat.id === categoryId)
+        return category ? category.name : 'Categoría no encontrada'
+    }
+
+    // Obtener los tags asociados a la imagen basado en el array de IDs
+    function getTags(tagIds: Array<number>) {
+        return tagIds.map(tagId => {
+            let tag = tags.find(t => t.id === tagId)
+            return tag ? tag.title : 'Tag no encontrado'
+        }).join(', ')
+    }
 </script>
 
-<!-- Tarjeta/Imagen de producto --><div>
-{#if images.length != 0}
+<!-- Tarjeta/Imagen de producto -->
+<div>
+    {#if images.length != 0}
         {#each images as image}
-        <!-- <div class="image-space-container"> -->
-            <div class="image-container {expandedImage === image.id ? 'expanded' : ''}" on:click={() => toggleImage(image.id)}>
+            <div class="image-container" on:click={() => openModal(image)}>
                 <figure class="image-item">
-                    <img class="image-product" src={Array.isArray(image.images) ? image.images[0] : image.images} alt={image.category}>
+                    <img class="image-product" src={Array.isArray(image.images) ? image.images[0] : image.images} alt={getCategoryName(image.category)}>
                 </figure>
-
-                <!-- Información del producto -->
-                {#if expandedImage === image.id}
-                <div class="product-info">
-                    <h3>{image.category}</h3>
-
-                    <h5>Precio</h5>
-                    <p>{image.price}</p>
-
-                    <h5>Tiempo</h5>
-                    <p>{image.time}</p>
-
-                    <h5>Características</h5>
-                    <p>{image.tags}</p>
-                </div>
-                {/if}
             </div>
-        <!-- </div> -->
         {/each}
+    {/if}
+
+  <!-- Modal para mostrar información del producto seleccionado -->
+  {#if showModal && selectedImage}
+  <div class="modal-overlay" on:click={closeModal}>
+      <div class="modal-content" on:click|stopPropagation>
+
+        <ul>
+            {#each images as image}
+                <li>
+                    <img class="image-carousel" src={Array.isArray(selectedImage.images) ? selectedImage.images[0] : selectedImage.images} alt={getCategoryName(selectedImage.category)}>
+                </li>
+            {/each}
+        </ul>
+
+          <h3>{getCategoryName(selectedImage.category)}</h3>
+          <h5>Precio</h5>
+          <p>{selectedImage.price}</p>
+          <h5>Tiempo</h5>
+          <p>{selectedImage.time} días</p>
+          <h5>Características</h5>
+          <p>{getTags(selectedImage.tags)}</p>
+          <button on:click={closeModal}>Cerrar</button>
+      </div>
+  </div>
 {/if}
 </div>
 
 
 <style lang='sass'>
 @use '../../styles/media' as media
-@use '../../styles/palete' as palete
+@use '../../styles/user/palete' as palete
 
-.image-space-container
-        display: flex
-        flex-wrap: wrap
-        justify-content: center
-        gap: 1em
+// .image-space-container
+//         display: flex
+//         flex-wrap: wrap
+//         justify-content: center
+//         gap: 1em
 
 .image-container
     position: relative
@@ -72,15 +115,10 @@
     height: 13em 
     display: flex
     flex-wrap: wrap
+    cursor: pointer
     transition: all 0.3s ease
         
 
-
-    &.expanded
-        width: 16em
-        height: auto
-        flex-wrap: wrap
-        
 
 figure
     width: 13em
@@ -106,30 +144,71 @@ figure
     width: 75%
     height: 75%
     object-fit: cover
-    border: 0.3px solid palete.$border-container
+    border: 0.3px solid palete.$u-border-container
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.7)
     z-index: 10
 
-    & .expanded
-        shadow:none
-
-    
-.focus
-    grid-column: auto / span 2
-    grid-row: auto /span 2
-
-
-.product-info
-    padding: 1em
-    background-color: #f4f4
-    border-radius: 8px
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2)
-    position: absolute
-    top: 0  // Alinea al inicio del contenedor
+.modal-overlay
+    position: fixed
+    top: 0
     left: 0
-    width: 100% 
+    width: 100vw
+    height: 100vh
+    background-color: rgba(0, 0, 0, 0.7)
+    display: flex
+    justify-content: center
+    align-items: center
+    z-index: 1000
 
+.modal-content
+    background-color: palete.$u-container
+    padding: 2em
+    border-radius: 8px
+    width: 90%
+    max-width: 500px
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2)
+    text-align: center
 
+    justify-content: columns
 
+    h5
+        color: palete.$u-title
+
+    p
+        color: palete.$u-text
+
+    button
+        width: 50%
+        height: 40px
+        font: 1em Nunito
+
+        border-radius: 10px
+
+        border: palete.$u-button-border
+        background: palete.$u-button
+
+        margin-top: 20px
+
+        transition: background 0.5s ease-in-out
+
+        &:hover
+            background: palete.$u-button-hover
+            cursor: pointer
+
+.image-carousel
+    border-radius: 5px
+    display:flex
+    position: relative
+    width: 20%
+    height: 20%
+    object-fit: cover
+    border: 0.3px solid palete.$u-border-container
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.7)
+    z-index: 10
+    overflow-x: auto
+
+    gap: 6px
+
+    scroll-snap-type: x mandatory
 
 </style>
